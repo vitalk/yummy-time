6f4a51563f511a212b97e4fecaabac8b67bd260c
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-17 18:23:19 +0300

    Reformat server routes to unsure they don’t exceed length 100 chars

--- server/config/routes.js
+++ server/config/routes.js
@@ -15,10 +15,14 @@ module.exports = function applyRoutes(app) {
   app.post('/auth/token', auth.token);
   app.post('/auth/google-oauth2', auth.google.callback);
 
-  app.get('/:type(orders|accounts|portions|vendors|products)', passport.authenticate('jwt'), api);
-  app.get('/:type(orders|accounts|portions|vendors|products)/:id', passport.authenticate('jwt'), api);
-  app.patch('/:type(orders|portions)/:id', passport.authenticate('jwt'), api);
-  app.post('/:type(orders|portions|vendors|products)', passport.authenticate('jwt'), api);
+  app.get('/:type(orders|accounts|portions|vendors|products)',
+          passport.authenticate('jwt'), api);
+  app.get('/:type(orders|accounts|portions|vendors|products)/:id',
+          passport.authenticate('jwt'), api);
+  app.patch('/:type(orders|portions)/:id',
+            passport.authenticate('jwt'), api);
+  app.post('/:type(orders|portions|vendors|products)',
+           passport.authenticate('jwt'), api);
   app.post('/:type(accounts)', api);
   app.patch('/:type(accounts)/:id', passport.authenticate('jwt'), api);
 };
