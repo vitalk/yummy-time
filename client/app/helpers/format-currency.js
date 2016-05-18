955ff84c3235fc785fa2c9a80a26dc00675f829a
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-18 10:03:41 +0300

    Allow reassing sep variable if it’s undefined

--- client/app/helpers/format-currency.js
+++ client/app/helpers/format-currency.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export function formatCurrency([number], { sep }) {
-  const sep = (sep === undefined) ? '&thinsp;' : sep;
+  if (typeof sep === 'undefined') {
+    // eslint-disable-next-line no-param-reassign
+    sep = '&thinsp;';
+  }
   const value = number.toString();
   const groups = value.match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g);
   return Ember.String.htmlSafe(groups.join(sep));
