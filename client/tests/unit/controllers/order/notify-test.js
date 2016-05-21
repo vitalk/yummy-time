0b76fd0925f7406c33ffe2dab51ee12dfa2c4ba7
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-21 11:36:14 +0300

    Rename checkout route into notify
    - Update related form component;
    
    - Rename form block as well.

---
+++ client/tests/unit/controllers/order/notify-test.js
@@ -0,0 +1,9 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:order/notify', 'Unit | Controller | order/notify', {
+});
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
