7d88764e08703b66fb808a975145cd8e2085e969
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-23 21:04:50 +0300

    Rename f-portion component into f-product
    Update stylesheet for consistency.

--- client/app/components/f-product.js
+++ client/app/components/f-product.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import { ProductActionsMixin } from '../controllers/order/index';
 
 export default Ember.Component.extend(ProductActionsMixin, {
+  tagName: 'form',
+  classNames: ['f-default', 'f-product'],
   order: null,
   products: null
 });
