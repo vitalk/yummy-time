fdeedd2da95ab50ed44afc4017148278e97f634b
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-23 21:31:38 +0300

    Allow filter products on products page

--- client/app/components/f-product.js
+++ client/app/components/f-product.js
@@ -5,5 +5,19 @@ export default Ember.Component.extend(ProductActionsMixin, {
   tagName: 'form',
   classNames: ['f-default', 'f-product'],
   order: null,
-  products: null
+  products: null,
+
+  filter: null,
+  filteredProducts: Ember.computed('products.[]', 'filter', function() {
+    const filter = this.get('filter');
+    const regexp = new RegExp(filter, 'i');
+
+    if (filter) {
+      return this.get('products').filter((product) => {
+        return regexp.test(product.get('name'));
+      });
+    }
+
+    return this.get('products');
+  })
 });
