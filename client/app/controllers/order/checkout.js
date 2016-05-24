357575b85a1d24b94a2dc3c15eafb4d8fbe1d569
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-24 10:47:12 +0300

    Wrap checkout into form and show total cost

--- client/app/controllers/order/checkout.js
+++ client/app/controllers/order/checkout.js
@@ -4,10 +4,20 @@ import { unique } from '../../helpers/unique';
 export default Ember.Controller.extend({
   order: Ember.computed.alias('model.order'),
   portions: Ember.computed.alias('model.portions'),
-  orderProducts: Ember.computed('order.portions.[]', 'order.portions.@each.deleted', function() {
-    const res = this.get('order.portions').filter((portion) => {
+
+  orderPortions: Ember.computed('order.portions.[]', 'order.portions.@each.deleted', function() {
+    return this.get('order.portions').filter((portion) => {
       return (!portion.get('deleted') && portion.get('paid'));
-    }).map((portion) => portion.get('product'));
+    })
+  }),
+  orderProducts: Ember.computed('orderPortions', function() {
+    const res = this.get('orderPortions').map((portion) => portion.get('product'));
     return unique(res, 'id');
+  }),
+
+  totalCost: Ember.computed('orderPortions', function() {
+    return this.get('orderPortions').reduce((sum, portion) => {
+      return sum + portion.get('cost');
+    }, 0);
   })
 });
