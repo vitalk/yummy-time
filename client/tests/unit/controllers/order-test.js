c19c47f0920aacb79c371da76bfe94e3900dede5
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-24 09:35:36 +0300

    Remove test suite for non-existent controller

--- client/tests/unit/controllers/order-test.js
+++
@@ -1,9 +0,0 @@
-import { moduleFor, test } from 'ember-qunit';
-
-moduleFor('controller:order', 'Unit | Controller | order', {
-});
-
-test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
-});
