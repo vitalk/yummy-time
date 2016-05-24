a6061d0ffc1e49730b43602f9661e00e9df6951c
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-24 14:02:24 +0300

    Embed list of affiliates into vendor model

--- client/app/models/vendor.js
+++ client/app/models/vendor.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   url: DS.attr('string'),
   minOrderCost: DS.attr('number'),
-  products: DS.hasMany('product')
+  products: DS.hasMany('product'),
+  affiliates: DS.attr()
 });
