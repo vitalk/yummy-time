86a2f656f4c5c959e2490b922fa070581c83b050
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-26 21:54:21 +0300

    Add custom blank state for my orders

--- client/app/controllers/account/orders.js
+++ client/app/controllers/account/orders.js
@@ -2,4 +2,5 @@ import Ember from 'ember';
 import { NavigationTabsMixin } from './index';
 
 export default Ember.Controller.extend(NavigationTabsMixin, {
+  foundNothing: Ember.computed.empty('model')
 });
