83b739e1be99d0c2ae3dd045cc32e35ab88efe73
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-27 15:44:41 +0300

    Show total cost of my products

--- client/app/controllers/account/products.js
+++ client/app/controllers/account/products.js
@@ -13,5 +13,11 @@ export default Ember.Controller.extend(NavigationTabsMixin, ProductActionsMixin,
   myProducts: Ember.computed('myPortions.[]', function() {
     const res = this.get('myPortions').map((portion) => portion.get('product'));
     return unique(res, 'id');
+  }),
+
+  totalCost: Ember.computed('myPortions.[]', function() {
+    return this.get('myPortions').reduce((sum, portion) => {
+      return sum + portion.get('cost');
+    }, 0);
   })
 });
