070ffcbd3645db16d9d8bd444b15f71727c64cc7
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-28 08:03:53 +0300

    Remove tab to account settings
    We’ve got a link in main menu.

--- client/app/controllers/account/index.js
+++ client/app/controllers/account/index.js
@@ -4,12 +4,11 @@ import Ember from 'ember';
 export const NavigationTabsMixin = Ember.Mixin.create({
   tabs: new Ember.A([
     Ember.Object.create({ endpoint: 'account.orders', title: 'Мои заказы' }),
-    Ember.Object.create({ endpoint: 'account.products', title: 'Мои блюда' }),
-    Ember.Object.create({ endpoint: 'account.index', title: 'Настройки' })
+    Ember.Object.create({ endpoint: 'account.products', title: 'Мои блюда' })
   ])
 });
 
-export default Ember.Controller.extend(NavigationTabsMixin, {
+export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
   actions: {
