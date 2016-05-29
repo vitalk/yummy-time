431074f318f6e60087144e3099a2b52e2fbcbb9e
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-29 14:28:13 +0300

    Show notify button only if order has recipients
    - Extract logic into mixin;
    
    - Refactor f-notification component to reuse mixin.
    
    Closes #53.

--- client/app/components/b-order-actions.js
+++ client/app/components/b-order-actions.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+export const OrderParticipantsMixin = Ember.Mixin.create({
+  participants: Ember.computed.mapBy('order.portions', 'owner'),
+  recipients: Ember.computed('participants', 'order.manager.id', function() {
+    // eslint-disable-next-line arrow-body-style
+    return this.get('participants').filter((participant) => {
+      return (participant.get('id') !== this.get('order.manager.id'));
+    });
+  }),
+  hasRecipients: Ember.computed.notEmpty('recipients')
+});
+
+export default Ember.Component.extend(OrderParticipantsMixin, {
   tagName: 'footer',
   classNames: ['b-order__actions'],
 
@@ -11,8 +22,8 @@ export default Ember.Component.extend({
             this.get('order.isReady') && !this.get('order.deleted'));
   }),
 
-  canNotify: Ember.computed('order.deleted', 'session.account.id', function() {
+  canNotify: Ember.computed('order.deleted', 'session.account.id', 'hasRecipients', function() {
     return (this.get('order.manager.id') === this.get('session.account.id') &&
-            !this.get('order.deleted'));
+            !this.get('order.deleted') && this.get('hasRecipients'));
   })
 });
