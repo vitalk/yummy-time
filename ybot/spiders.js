8cc176a2a9f6488caf66fff9be6d323d29848ce2
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-06-07 20:13:46 +0300

    Remove optional spider name from init
    The canonical spider name is already defined into the constructor.

--- ybot/spiders.js
+++ ybot/spiders.js
@@ -4,6 +4,6 @@ const EburgerSpider = require('./spiders/eburger');
 const CityFoodSpider = require('./spiders/city-food');
 
 module.exports = [
-  new EburgerSpider('eburger'),
-  new CityFoodSpider('city-food')
+  new EburgerSpider,
+  new CityFoodSpider
 ];
